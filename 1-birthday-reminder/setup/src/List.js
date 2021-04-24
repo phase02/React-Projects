bdd80f4abb319f19ad75109ac56da6d75005f944
PoojaDev1510 <poojahosabettu@gmail.com>
2021-04-24 19:54:22 +0530

    Birthday project finished

---
+++ 1-birthday-reminder/setup/src/List.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((item) => {
+        const { id, name, age, image } = { ...item };
+        return <article key={id} className="person">
+          <img src={image} alt={name} />
+          <div>
+            <h4>{name}</h4>
+            <p>{age} years</p>
+          </div>
+        </article>
+      })}
+    </>
+  );
+};
+
+export default List;
